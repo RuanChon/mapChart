490a572c43316bd85d2b32c567a020ea61ac4090
chon <724082712@qq.com>
2020-06-02 15:03:53 +0800

    Echarts map下钻至县级

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <mapChart />
+  </div>
+</template>
+
+<script>
+import mapChart from './components/mapChart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    mapChart
+  }
+}
+</script>
+
+<style>
+
+</style>
